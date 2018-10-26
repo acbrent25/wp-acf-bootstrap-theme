c09f3c708bbc08d8eacd5ca797ba1be075ce4f54
Adam Champagne <achampagne@sussmanconsultants.com>
2018-10-26 15:30:19 -0400

    jarallax support

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -19,6 +19,8 @@ function ac_scripts() {
    wp_enqueue_script('popper-js', 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js', array('jquery'), '1.14.3', true);
    wp_enqueue_script('bootstrap-js', 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js', array('jquery'), '4.1.4', true);
    wp_enqueue_script( 'isotope-js', get_template_directory_uri() . '/js/isotope.js', array(), '20151215', true );
+   wp_enqueue_script('jaralax-js', 'https://unpkg.com/jarallax@1.10/dist/jarallax.min.js', array(), '1.1.0', true);
+
    
    /* Enqueue Scripts */
 	wp_enqueue_script( 'ac-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '3.0.6', true );
@@ -27,6 +29,8 @@ function ac_scripts() {
    wp_enqueue_script('jquery');
    wp_enqueue_script('popper-js');
    wp_enqueue_script('bootstrap-js');
+   wp_enqueue_script('isotope-js');
+   wp_enqueue_script('jaralax-js');
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
