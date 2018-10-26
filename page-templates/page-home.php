25e7917eae6f51eb25fd4e6bdc44d4e9cae8dec6
Adam Champagne <achampagne@sussmanconsultants.com>
2018-10-26 15:56:45 -0400

    added jumbotron component

---
+++ page-templates/page-home.php
@@ -0,0 +1,19 @@
+<?php
+/** Template Name: Home Page
+ * @package ACF-BS_Ultimate
+ */
+
+get_header();
+?>
+
+<?php 
+get_template_part('./template-parts/section-jumbotron/jumbotron','1'); 
+get_template_part('./template-parts/section-jumbotron/jumbotron','2'); 
+
+
+
+
+?>
+
+<?php
+get_footer();
