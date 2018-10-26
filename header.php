a3ae82490d1e04110577c2d2ddc1654450e2a1a8
Adam Champagne <achampagne@sussmanconsultants.com>
2018-10-26 15:41:36 -0400

    added custom menu function to header

--- header.php
+++ header.php
@@ -25,34 +25,36 @@
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'ac' ); ?></a>
 
 	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$ac_description = get_bloginfo( 'description', 'display' );
+
+		<nav class="navbar navbar-expand-md navbar-light bg-light" role="navigation">
+      <div class="container">
+         <!-- Brand and toggle get grouped for better mobile display -->
+         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-controls="bs-example-navbar-collapse-1" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+         </button>
+         <?php
+			the_custom_logo(); ?>
+         <a class="navbar-brand" href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a>
+         <?php 
+         $ac_description = get_bloginfo( 'description', 'display' );
 			if ( $ac_description || is_customize_preview() ) :
 				?>
-				<p class="site-description"><?php echo $ac_description; /* WPCS: xss ok. */ ?></p>
+				<!-- <p class="site-description"><?php echo $ac_description; /* WPCS: xss ok. */ ?></p> -->
 			<?php endif; ?>
-		</div><!-- .site-branding -->
-
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'ac' ); ?></button>
-			<?php
-			wp_nav_menu( array(
-				'theme_location' => 'menu-1',
-				'menu_id'        => 'primary-menu',
-			) );
-			?>
-		</nav><!-- #site-navigation -->
+            <?php
+            wp_nav_menu( array(
+               'theme_location'    => 'menu-1',
+               'depth'             => 2,
+               'container'         => 'div',
+               'container_class'   => 'collapse navbar-collapse',
+               'container_id'      => 'bs-example-navbar-collapse-1',
+               'menu_class'        => 'nav navbar-nav ml-auto',
+               'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
+               'walker'            => new WP_Bootstrap_Navwalker(),
+            ) );
+            ?>
+         </div>
+      </nav>
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
